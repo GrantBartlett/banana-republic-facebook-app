3902e3717f39686813489b41e01d59200c501090
Grant Bartlett <hello@grant-bartlett.com>
2015-11-12 16:44:03 +0000

    https support

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 var error = require('./lib/errors');
+var https = require('https');
+var fs = require('fs');
 
 // Routes
 var form = require('./routes/form');
@@ -11,15 +13,25 @@ var form = require('./routes/form');
 var server = app.listen(8000, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Banana Republic App listening at http://%s:%s', host, port);
 });
 
+// HTTPS Server
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+}, app).listen(8443, function () {
+    var host = this.address().address;
+    var port = this.address().port;
+    console.log('Banana Republic App listening at https://%s:%s', host, port);
+});
+
+// Setup public folder routing
 app.use(express.static(__dirname + '/../../dist/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Form
+// Form endpoint
 app.post('/form', form.save);
 
 // Error handling
