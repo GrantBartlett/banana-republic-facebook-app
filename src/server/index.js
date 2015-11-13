5859bf53373472820622c6d90e06561a2dc6f729
Grant Bartlett <hello@grant-bartlett.com>
2015-11-13 15:01:13 +0000

    Remove err handler for now, not really needed

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var error = require('./lib/errors');
 var https = require('https');
 var fs = require('fs');
 var path = require('path');
@@ -9,24 +8,21 @@ var path = require('path');
 // Routes
 var form = require('./routes/form');
 
+// Heroku sets port dynamically, fallback for dev env
 app.set('port', (process.env.PORT || 5000));
 
 // Setup public folder routing
-app.use(express.static(__dirname + '/../../dist/public'));
+app.use(express.static(path.resolve('dist/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Facebook POST route
 app.post('/*', function (req, res) {
     res.sendFile(path.resolve('dist/public/index.html'));
-    //res.sendFile(__dirname + '/index.html');
 });
 
 // Form endpoint
 app.post('/form', form.save);
-
-// Error handling
-app.use(error.clientErrorHandler);
 
 // Server
 var server = app.listen(app.get('port'), function () {
--- src/server/lib/errors.js
+++
@@ -1,7 +0,0 @@
-exports.clientErrorHandler = function (err, req, res, next) {
-    if (req.xhr) {
-        res.status(500).send({error: 'Something blew up!'});
-    } else {
-        next(err);
-    }
-};
