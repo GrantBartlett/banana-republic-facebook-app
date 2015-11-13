2684515d8edc170ff4635ac03b19b0db005c6681
Grant Bartlett <hello@grant-bartlett.com>
2015-11-13 13:31:49 +0000

    ../.. is considered malicious, use path.resolve

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var error = require('./lib/errors');
 var https = require('https');
 var fs = require('fs');
+var path = require('path');
 
 // Routes
 var form = require('./routes/form');
@@ -14,6 +15,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/../../dist/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Facebook POST route
+app.post('/*', function (req, res) {
+    res.sendFile(path.resolve('dist/public/index.html'));
+    //res.sendFile(__dirname + '/index.html');
+});
 
 // Form endpoint
 app.post('/form', form.save);
@@ -26,9 +33,4 @@ var server = app.listen(app.get('port'), function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Banana Republic App listening at http://%s:%s', host, port);
-});
-
-// Facebook POST route
-app.post('/*', function (req, res) {
-    res.sendfile(__dirname + '/../../dist/public/index.html');
 });
